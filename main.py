78a25c41c259826de974820b0ccd575a2cb97956
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2022-10-07 10:12:11 +0100

    Changed file names and added initial game variables

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import pygame, sys, os
+import random
 from pygame.locals import *
 pygame.init()
+
 
 pygame.display.set_caption('Black Jack')
 WIDTH,HEIGHT = 600, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
 
+playerCardValue = 0
+computerCardValue = 0
+
+playerCardInitialPos_X, playerCardInitialPos_Y = 100, 500
+computerCardInitialPos_X, computerCardInitialPos_Y = 100, 200
+
+playerCardPosMultiplier_X = 5;
+computerCardPosMultiplier_X = 5;
+
+playerNumberOfCards = 0
+computerNumberOfCards = 0
 
 #Load Images (Global)
 path = 'assets'
@@ -31,50 +44,50 @@ aceD = Card('ace_diamonds', 1, ace_diamonds)
 aceS = Card('ace_spades', 1, ace_spades)
 aceC = Card('ace_clubs', 1, ace_clubs)
 
-2H = Card('2_hearts', 2, 2_hearts)
-2D = Card('2_diamonds', 2, 2_diamonds)
-2S = Card('2_spades', 2, 2_spades)
-2C = Card('2_clubs', 2, 2_clubs)
+twoH = Card('2_hearts', 2, two_hearts)
+twoD = Card('2_diamonds', 2, two_diamonds)
+twoS = Card('2_spades', 2, two_spades)
+twoC = Card('2_clubs', 2, two_clubs)
 
-3H = Card('3_hearts', 3, 3_hearts)
-3D = Card('3_diamonds', 3, 3_diamonds)
-3S = Card('3_spades', 3, 3_spades)
-3C = Card('3_clubs', 3, 3_clubs)
+threeH = Card('3_hearts', 3, three_hearts)
+threeD = Card('3_diamonds', 3, three_diamonds)
+threeS = Card('3_spades', 3, three_spades)
+threeC = Card('3_clubs', 3, three_clubs)
 
-4H = Card('4_hearts', 4, 4_hearts)
-4D = Card('4_diamonds', 4, 4_diamonds)
-4S = Card('4_spades', 4, 4_spades)
-4C = Card('4_clubs', 4, 4_clubs)
+fourH = Card('4_hearts', 4, four_hearts)
+fourD = Card('4_diamonds', 4, four_diamonds)
+fourS = Card('4_spades', 4, four_spades)
+fourC = Card('4_clubs', 4, four_clubs)
 
-5H = Card('5_hearts', 5, 5_hearts)
-5D = Card('5_diamonds', 5, 5_diamonds)
-5S = Card('5_spades', 5, 5_spades)
-5C = Card('5_clubs', 5, 5_clubs)
+fiveH = Card('5_hearts', 5, five_hearts)
+fiveD = Card('5_diamonds', 5, five_diamonds)
+fiveS = Card('5_spades', 5, five_spades)
+fiveC = Card('5_clubs', 5, five_clubs)
 
-6H = Card('6_hearts', 6, 6_hearts)
-6D = Card('6_diamonds', 6, 6_diamonds)
-6S = Card('6_spades', 6, 6_spades)
-6C = Card('6_clubs', 6, 6_clubs)
+sixH = Card('6_hearts', 6, six_hearts)
+sixD = Card('6_diamonds', 6, six_diamonds)
+sixS = Card('6_spades', 6, six_spades)
+sixC = Card('6_clubs', 6, six_clubs)
 
-7H = Card('7_hearts', 7, 7_hearts)
-7D = Card('7_diamonds', 7, 7_diamonds)
-7S = Card('7_spades', 7, 7_spades)
-7C = Card('7_clubs', 7, 7_clubs)
+sevenH = Card('7_hearts', 7, seven_hearts)
+sevenD = Card('7_diamonds', 7, seven_diamonds)
+sevenS = Card('7_spades', 7, seven_spades)
+sevenC = Card('7_clubs', 7, seven_clubs)
 
-8H = Card('8_hearts', 8, 8_hearts)
-8D = Card('8_diamonds', 8, 8_diamonds)
-8S = Card('8_spades', 8, 8_spades)
-8C = Card('8_clubs', 8, 8_clubs)
+eightH = Card('8_hearts', 8, eight_hearts)
+eightD = Card('8_diamonds', 8, eight_diamonds)
+eightS = Card('8_spades', 8, eight_spades)
+eightC = Card('8_clubs', 8, eight_clubs)
 
-9H = Card('9_hearts', 9, 9_hearts)
-9D = Card('9_diamonds', 9, 9_diamonds)
-9S = Card('9_spades', 9, 9_spades)
-9C = Card('9_clubs', 9, 9_clubs)
+nineH = Card('9_hearts', 9, nine_hearts)
+nineD = Card('9_diamonds', 9, nine_diamonds)
+nineS = Card('9_spades', 9, nine_spades)
+nineC = Card('9_clubs', 9, nine_clubs)
 
-10H = Card('10_hearts', 10, 10_hearts)
-10D = Card('10_diamonds', 10, 10_diamonds)
-10S = Card('10_spades', 10, 10_spades)
-10C = Card('10_clubs', 10, 10_clubs)
+tenH = Card('10_hearts', 10, ten_hearts)
+tenD = Card('10_diamonds', 10, ten_diamonds)
+tenS = Card('10_spades', 10, ten_spades)
+tenC = Card('10_clubs', 10, ten_clubs)
 
 JH = Card('J_hearts', 11, jack_hearts)
 JD = Card('J_diamonds', 11, jack_diamonds)
@@ -95,8 +108,21 @@ KC = Card('K_clubs', 13, king_clubs)
 
 
 def drawWindow():
-    WIN.blit(board, (0,0))
+    #WIN.blit(board, (0,0))
     pygame.display.update()
+    
+
+def playerHit():
+    playerCard = random.choice(listOfCards)
+    if playerNumberOfCards == 0:
+        WIN.blit(playerCard.image, (playerCardInitialPos_X, playerCardInitialPos_Y))
+        playerNumberOfCards = playerNumberOfCards + 1
+    else:
+        WIN.blit(playerCard.image, (playerCardInitialPos_X + (), playerCardInitialPos_Y))
+    
+
+def computerHit():
+    computerCard = random.choice(listOfCards)
 
 def main():
     clock = pygame.time.Clock()
